36744df045091f05cce0cc99469cac2610baef00
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-06-25 03:28:22 +0530

    Testing Drag effect

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string text = null;
+	Vector2 temp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i=0; i<Input.touchCount; i++) {
+			Touch touch = Input.GetTouch(i);
+
+			if(touch.phase == TouchPhase.Began) {
+				Vector2 position = Camera.main.ScreenToWorldPoint(touch.position);
+				temp.x = transform.position.x - position.x;
+				temp.y = transform.position.y - position.y;
+			}
+
+//			if(touch.phase == TouchPhase.Ended && touch.tapCount == 1) {
+//				text = "Tap";
+//				Vector2 position = Camera.main.ScreenToWorldPoint(touch.position);
+//				transform.position = position;
+//			}
+
+			if(touch.phase == TouchPhase.Moved) {
+				text = "Drag";
+				Vector2 position = Camera.main.ScreenToWorldPoint(touch.position);
+				transform.position = position + temp;
+
+			}
+		}
+	}
+}
---
+++ Assets/Touch_Control.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Touch_Control : MonoBehaviour {
+
+	Text text;
+	Player player;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+		player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(text.text != player.text) {
+			text.text = player.text;
+		}
+	}
+}
